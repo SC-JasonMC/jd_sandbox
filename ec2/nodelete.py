f259d0fd7403cef1752ea1cb22d2a43ced2a2047
Jason McLeod <jasonmc@softcat.com>
2026-07-10 16:50:17 +0100

    Test

--- ec2/nodelete.py
+++ ec2/nodelete.py
@@ -16,14 +16,4 @@ with open(csv_path, mode='r') as file:
             DisableApiTermination={'Value': True}
     )
         
-# Adding text to test state machine.
 
-# Second attempt to trigger eventbridge.
-
-# Second attempt worked with no .zip filter. Adding that back in to see if I can get it working.
-
-# Removed trailing white space from EventBridge S3 bucket name filter.
-
-# Adding another line to test Lambda stack MVP
-
-# GTesting agaiun
